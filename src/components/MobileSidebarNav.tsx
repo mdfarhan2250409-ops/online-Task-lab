b450f23c1d1d7128fa72480bc5873f8d89c21f68
mdfarhan2250409-ops <md.farhan2250409@gmail.com>
2026-08-13 12:45:59 +0600

    refactor: migrate from HashRouter to BrowserRouter
    Switch the application router to BrowserRouter to support cleaner URLs and standard browser navigation patterns.

--- src/components/MobileSidebarNav.tsx
+++ src/components/MobileSidebarNav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useApp } from '../context/AppContext';
 import { motion, AnimatePresence } from 'motion/react';
 import { OtlLogo } from './OtlLogo';
